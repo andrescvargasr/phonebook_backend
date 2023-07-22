0ebd3451fefbc990d2cbc5a953184de6b7326e40
andrescvargasr <andresc.vargasr@gmail.com>
2023-07-22 17:47:50 +0200

    feat: ex 3.4 -> add .rest file for test in VSCode

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.json())
 
 const PORT = 3001
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -48,6 +48,14 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
 })
 
 app.listen(PORT, () => {
